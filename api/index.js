fc518bd04dc279d236a9779795e4d9c09a5c83ed
illes <illeszarouri@gmail.com>
2024-03-07 00:25:16 +0100

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => console.log('connect to db'));
 
 const app = express();
 
-app.listen(300, () => (console.log("this nigga working!!!")))
+app.listen(300, () => (console.log("this nigga working!!!")));
