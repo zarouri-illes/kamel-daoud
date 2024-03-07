7bd5b6e46fc03c3ddd8e6a5b5693718c55eb6b09
illes <illeszarouri@gmail.com>
2024-03-07 03:00:33 +0100

    create auth

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -9,6 +10,9 @@ mongoose.connect(process.env.MONGO).then(() => console.log('connect to db'));
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => (console.log("this nigga working!!!")));
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
